5b8ff1b271669d899600d6f1d77b0060140e29f0
Christian Berendt <berendt@osism.tech>
2023-12-18 14:56:56 +0100

    Remove duplicate title from home page
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,6 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title}`}
       description="Regionale Cloud">
       <HomepageHeader />
       <main>
